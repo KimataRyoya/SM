f36a4367f7cc3e6670af89b53fe75a0698f6512d
Kimata <kimataroudayoo@yahoo.co.jp>
2025-03-17 19:05:11 +0900

    絞り込み検索を実装

--- src/main/java/raisetech/student/management/service/StudentService.java
+++ src/main/java/raisetech/student/management/service/StudentService.java
@@ -1,6 +1,10 @@
 package raisetech.student.management.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import raisetech.student.management.data.Student;
@@ -19,8 +23,9 @@ public class StudentService {
 
   public List<Student> searchStudentList() {
     //　検索処理
-    return repository.search();
-
+    return repository.search().stream()
+        .filter(student -> student.getAge() >= 30)
+        .collect(Collectors.toList());
     //　絞り込みをする。年齢が30代以上の人のみを抽出する。
     //　抽出したリストをコントローラーに返す。
   }
@@ -28,6 +33,8 @@ public class StudentService {
   public List<StudentCourses> searchStudentCoursesList() {
     //　絞り込み検索で「Javaコース」のコース情報のみを抽出する。
     //　抽出したリストをコントローラーに返す。
-    return repository.searchCourses();
+    return repository.searchCourses().stream()
+        .filter(studentCourses -> studentCourses.getCourses().contains("Java"))
+        .collect(Collectors.toList());
   }
 }
