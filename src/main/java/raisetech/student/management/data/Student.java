3c36bbcaf321dfeb8a18cea3cf0d0aa1d256b70a
Kimata <kimataroudayoo@yahoo.co.jp>
2025-04-22 19:49:06 +0900

    受講生情報登録ができない

--- src/main/java/raisetech/student/management/data/Student.java
+++ src/main/java/raisetech/student/management/data/Student.java
@@ -1,12 +1,20 @@
 package raisetech.student.management.data;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Getter
 @Setter
 public  class  Student {
 
+  @Id
   private String id;
   private String name;
   private String furigana;
@@ -17,5 +25,7 @@ public  class  Student {
   private String gender;
   private String remark;
   private boolean isDeleted;
+
+
 
 }
